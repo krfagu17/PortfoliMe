c666d4318a5a009f031c2e933c48ed7ca356a201
krfagu17 <krfagu17@gmail.com>
2023-12-07 16:10:39 +0530

    all done

--- src/components/info/Info.jsx
+++ src/components/info/Info.jsx
@@ -27,24 +27,24 @@ const Info = () => {
         
         className="icon-section">
 
-        <motion.img initial={{ x:100 ,opacity: 0}}
-        whileInView={{x:0, opacity: 1}}
+        <motion.img initial={{ y:-100 ,opacity: 0}}
+        whileInView={{y:0, opacity: 1}}
         transition={{duration: 0.4}} src="./infoAssets/html5.svg" />
-        <motion.img initial={{ x:120 ,opacity: 0}}
-        whileInView={{x:0, opacity: 1}}
+        <motion.img initial={{ y:120 ,opacity: 0}}
+        whileInView={{y:0, opacity: 1}}
         transition={{duration: 0.6}} src="./infoAssets/css3.svg" />
-        <motion.img initial={{ x:140 ,opacity: 0}}
-        whileInView={{x:0, opacity: 1}}
+        <motion.img initial={{ y:-140 ,opacity: 0}}
+        whileInView={{y:0, opacity: 1}}
         transition={{duration: 0.8}} src="./infoAssets/react-js.svg" />
-        <motion.img initial={{ x:160 ,opacity: 0}}
-        whileInView={{x:0, opacity: 1}}
-        transition={{duration: 1}} src="./infoAssets/nodejs.svg" />
-        <motion.img initial={{ x:180 ,opacity: 0}}
-        whileInView={{x:0, opacity: 1}}
-        transition={{duration: 1.2}} src="./infoAssets/vs-code.svg" />
-        <motion.img initial={{ x:100 ,opacity: 0}}
-        whileInView={{x:0, opacity: 1}}
-        transition={{duration: 1.4}} src="./infoAssets/react-native.svg" />
+        <motion.img initial={{ y:160 ,opacity: 0}}
+        whileInView={{y:0, opacity: 1}}
+        transition={{duration: 0.4}} src="./infoAssets/nodejs.svg" />
+        <motion.img initial={{ y:-180 ,opacity: 0}}
+        whileInView={{y:0, opacity: 1}}
+        transition={{duration: 0.6}} src="./infoAssets/vs-code.svg" />
+        <motion.img initial={{ y:200 ,opacity: 0}}
+        whileInView={{y:0, opacity: 1}}
+        transition={{duration: 0.8}} src="./infoAssets/react-native.svg" />
         </motion.div>
     </motion.div>
   )
