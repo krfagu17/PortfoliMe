41eabbf5b67469e1b4847c3be60ff8b624d9a08c
krfagu17 <krfagu17@gmail.com>
2023-12-09 08:54:28 +0530

    try 2

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import dotenv from 'dotenv';
 
+
+
+const envFiles = {
+  development: '.env.local',
+  production: '.env.production',
+};
+
+dotenv.config({
+  path: envFiles[process.env.NODE_ENV] || '.env',
+});
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
